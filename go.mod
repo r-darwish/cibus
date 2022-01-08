ac2765f1c3cc4e7826945e15a4e05a7c8eaae766
darwish <roey.dror@wiz.io>
2022-01-08 10:43:53 +0200

    Password prompt

--- go.mod
+++ go.mod
@@ -2,10 +2,13 @@ module github.com/wiz-sec/cibus
 
 go 1.17
 
-require github.com/chromedp/chromedp v0.7.6
+require (
+	github.com/chromedp/cdproto v0.0.0-20211126220118-81fa0469ad77
+	github.com/chromedp/chromedp v0.7.6
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211
+)
 
 require (
-	github.com/chromedp/cdproto v0.0.0-20211126220118-81fa0469ad77 // indirect
 	github.com/chromedp/sysutil v1.0.0 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
--- main.go
+++ main.go
@@ -1,22 +1,52 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"errors"
+	"fmt"
 	"github.com/wiz-sec/cibus/internal"
+	"golang.org/x/term"
 	"log"
 	"os"
+	"strings"
+	"syscall"
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		log.Fatalf("Usage: cibus [username] [password]")
+	username, password, err := GetCredentials()
+	if err != nil {
+		log.Fatalf("Unable to retrieve credentials: %s", err)
 	}
 
-	err := internal.AddAllFriends(os.Args[1], os.Args[2])
+	err = internal.AddAllFriends(username, password)
 	if err != nil {
 		if !errors.Is(err, context.DeadlineExceeded) {
 			log.Fatalf("%s", err.Error())
 		}
+	}
+}
+
+func GetCredentials() (username, password string, err error) {
+	if len(os.Args) != 3 {
+		reader := bufio.NewReader(os.Stdin)
+
+		fmt.Print("Enter Username: ")
+		username, err := reader.ReadString('\n')
+		if err != nil {
+			return "", "", err
+		}
+
+		fmt.Print("Enter Password: ")
+		bytePassword, err := term.ReadPassword(int(syscall.Stdin))
+		if err != nil {
+			return "", "", err
+		}
+
+		password := string(bytePassword)
+		return strings.TrimSpace(username), password, nil
+
+	} else {
+		return os.Args[1], os.Args[2], nil
 	}
 }
